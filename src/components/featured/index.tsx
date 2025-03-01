28d3f245679252817b1abdbba48830930da87e08
arzljames <arzljames15@gmail.com>
2025-03-01 23:15:47 +0800

    initial commit

---
+++ src/components/featured/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Featured() {
+  return <div>index</div>;
+}
+
+export default Featured;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": "/src/components",
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
