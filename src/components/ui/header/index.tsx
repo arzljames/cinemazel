634d5d19c2a31264ad5f3a5388dd796bce6af0bb
arzljames <arzljames15@gmail.com>
2025-04-07 21:23:04 +0800

    remove classNames

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -3,7 +3,7 @@ import BrandName from "../../../assets/branding/brand_name.png";
 
 const Header = () => {
   return (
-    <div className="pl-12 p-36 bg-red-50 flex justify-between mx-10">
+    <div>
       <h1>
         <img src={BrandName} alt="brand-name" />
       </h1>
